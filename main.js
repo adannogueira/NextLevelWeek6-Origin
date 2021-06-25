5d232f1c768176c3ff9d007afa082f080ca8762b
adannogueira <adan.nogueira@gmail.com>
2021-06-25 13:13:52 -0300

    finaliza o projeto

--- main.js
+++ main.js
@@ -85,7 +85,22 @@ const sections = document.querySelectorAll('main section[id]')
 function menuActivCurrentSection() {
   const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
 
-  
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document.querySelector('nav ul li a[href*= ' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document.querySelector('nav ul li a[href*= ' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
 }
 
 // Window Scroll Listener
